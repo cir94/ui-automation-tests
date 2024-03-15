493c35346fdc46c12476fa73ea4de38779da3130
cir94 <claire.isabella.ramos@gmail.com>
2024-03-15 11:45:44 -0400

    feat: Finished hidden layers test

--- tests/testing-playground-hiddenlayers.js
+++ tests/testing-playground-hiddenlayers.js
@@ -1,14 +1,15 @@
 import { envs } from '../url-module.js';
 
-// This particular test is unfinished. While it can't click the button twice, it still returns a failed test. Researching how to turn failed test to positive
 describe('UI Testing Playground - Hidden Layers', function() {
-    it('should click the green button only once', async function() {
+    it('should click the green button once, then blue button which appears over green, and not return to green', async function() {
         envs.uiTestingUrl.pathname = '/hiddenlayers';
         await browser.url(`${envs.uiTestingUrl}`);
         const greenButton = browser.$('#greenButton');
+        const blueButton = browser.$('#blueButton');
         await greenButton.waitForClickable();
         await greenButton.click();
-        browser.pause(1000);
-        await greenButton.click();
-    })
-})
+        expect(blueButton).toBeClickable();
+        await blueButton.click();
+        await greenButton.waitForClickable( { reverse: true });
+    });
+});
