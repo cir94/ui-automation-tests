3803c33dfa11fc9af3e7b5955ad5f82d22b6438a
cir94 <claire.isabella.ramos@gmail.com>
2024-03-18 22:28:36 -0400

    feat: finished visibility test, created page to house variables for test

--- tests/testing-playground-visibility.js
+++ tests/testing-playground-visibility.js
@@ -1,13 +1,20 @@
 import { envs } from '../url-module.js';
+import VisibilityPage from '../pages/visibility-page.js';
+// VisibilityPage is being used for readability sake. A lot of variables need to be defined, and defining them in a page file to then be imported keeps the code looking clean
 
 describe('UI Testing Playground - Visibility', function() {
-    it('should be able to click on hide button', async function() {
+    it('should be able to click on hide button and hide all other buttons on the page', async function() {
         envs.uiTestingUrl.pathname = '/visibility';
-        await browser.$(`${envs.uiTestingUrl}`);
-        const hideBtn = browser.$('#hideButton');
-        const removedBtn = browser.$('#removedButton');
-        await hideBtn.waitForClickable();
-        await hiddeBtn.click();
-        await expect(removedBtn).not.toBeDisplayed();
+        await browser.url(`${envs.uiTestingUrl}`);
+        await VisibilityPage.hideBtn.waitForClickable();
+        await VisibilityPage.hideBtn.click();
+        // The code below will check for all buttons to be hidden
+        await VisibilityPage.removeBtn.waitForClickable({ reverse: true });
+        await VisibilityPage.zeroBtn.waitForClickable({ reverse: true });
+        await VisibilityPage.overlapBtn.waitForClickable({ reverse: true });
+        await VisibilityPage.transparentBtn.waitForClickable({ reverse: true });
+        await VisibilityPage.invisibleBtn.waitForClickable({ reverse: true });
+        await VisibilityPage.noDisplayBtn.waitForClickable({ reverse: true });
+        await VisibilityPage.offscreenBtn.waitForClickable({ reverse: true });
     })
 })
