c181f6f2bc6161f69b901bad80d52fe38754f30a
cir94 <claire.isabella.ramos@gmail.com>
2024-03-20 11:04:39 -0400

    feat: created ajax cucumber test

---
+++ features/step_definitions/testing-playground-ajax-steps.js
@@ -0,0 +1,18 @@
+import { envs } from '../../url-module.js';
+import { Given, When, Then } from '@wdio/cucumber-framework';
+import Page from '../../pages/page.js';
+
+Given('the webpage is opened', async function() {
+    envs.uiTestingUrl.pathname = '/ajax'
+    await browser.url(`${envs.uiTestingUrl}`);
+});
+
+When('the AJAX button is clicked', async function() {
+    await expect(Page.button).toBeClickable();
+    await Page.button.click();
+});
+
+Then('an element should appear confirming data loaded', async function() {
+    await Page.bgSuccess.waitForExist({ timeout: 20000 });
+    await expect(Page.bgSuccess).toBeDisplayed();
+});
---
+++ features/testing-playground-ajax.feature
@@ -0,0 +1,6 @@
+Feature: Element appearing after processing AJAX Request
+
+    Scenario: Clicking button to start AJAX request
+    Given the webpage is opened
+    When the AJAX button is clicked
+    Then an element should appear confirming data loaded
