38f6a520778db17043124a584f6e62a899b80d1f
cir94 <claire.isabella.ramos@gmail.com>
2024-03-21 10:40:54 -0400

    fix: fixed mouseover test and feature to use given-steps

--- features/step_definitions/testing-playground-mouseover-steps.js
+++ features/step_definitions/testing-playground-mouseover-steps.js
@@ -1,12 +1,6 @@
-import { envs } from '../../url-module.js';
 import MouseOverPage from '../../pages/cucumber-pages/mouseover-page.js';
 import { Given, When, Then } from '@wdio/cucumber-framework';
-
-Given('the webpage is opened', async function() {
-    envs.uiTestingUrl.pathname = '/mouseover';
-    await browser.url(`${envs.uiTestingUrl}`);
-});
-
+ 
 When('the element is hovered and its class name changes', async function() {
     await MouseOverPage.inactiveText.waitForClickable();
     await expect(MouseOverPage.inactiveText).toBeClickable();
--- features/testing-playground-mouseover.feature
+++ features/testing-playground-mouseover.feature
@@ -1,6 +1,6 @@
 Feature: Clicking on button even after element is modified
 
     Scenario: Recording 2 consecutive clicks on element
-        Given the webpage is opened
+        Given I open the url "http://uitestingplayground.com/mouseover"
         When the element is hovered and its class name changes 
         Then I should be able to click it again
