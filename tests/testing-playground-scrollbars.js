fda005e4308630b79f69d8684c8f4e94b1215bc5
cir94 <claire.isabella.ramos@gmail.com>
2024-04-01 15:18:41 -0400

    feat: created Scrollbars test for UI Playground

---
+++ tests/testing-playground-scrollbars.js
@@ -0,0 +1,12 @@
+import { envs } from '../url-module.js';
+
+describe('UI Testing Playground - Scrollbars', function () {
+  it('should scroll the button into view and click it', async function () {
+    envs.uiTestingUrl.pathname = '/scrollbars';
+    await browser.url(`${envs.uiTestingUrl}`);
+    const hiddenButton = $('#hidingButton');
+    await expect(hiddenButton).toBeClickable();
+    await hiddenButton.scrollIntoView({ block: 'center', inline: 'center' });
+    await hiddenButton.click();
+  });
+});
