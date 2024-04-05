69286ce2b463250bf6429fdabd22971269609709
cir94 <claire.isabella.ramos@gmail.com>
2024-04-04 21:36:01 -0400

    feat: edited eslintrc to have unused vars return as warnings due to cucumber given returning as unused

--- features/step_definitions/testing-playground-shadowdom-steps.js
+++ features/step_definitions/testing-playground-shadowdom-steps.js
@@ -1,4 +1,4 @@
-import { Given, When, Then } from '@wdio/cucumber-framework';
+import { Given, When, Then } from '@wdio/cucumber-framework'; //eslint-disable-line no-unused-vars
 import ShadowDomPage from '../../pages/cucumber-pages/shadowdom-page.js';
 
 // The Features file associated with this test is commented out due to an uncertified insecure certificate from the website this test points to, rendering it unable to complete.
@@ -13,7 +13,7 @@ When('the generate button is clicked and it creates a string', async function() 
 
 Then('the copy button can be pressed to copy it to the clipboard', async function() {
     await ShadowDomPage.copyButton.click();
-    const fieldText = await guidField.getValue();
+    const fieldText = await ShadowDomPage.guidField.getValue();
     await expect(await window.navigator.clipboard.readText()).toHaveClipboardText(
         expect.stringContaining(fieldText)
       );
